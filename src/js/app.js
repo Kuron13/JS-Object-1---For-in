5e3e057ff09466271642d1d93166eefe954359c5
Tsvetkova Victoria <LeukocyteGin@yandex.ru>
2025-01-29 23:32:13 +0500

    Task 1

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,32 @@
 // TODO: write your code here
-import sum from './basic';
 
 console.log('worked');
 
-console.log(sum([1, 2]));
+export default function sortCharStates (obj, reference) {
+  let objToList = []
+
+  for (const state in obj) {
+    objToList.push({'key': state, 'value': obj[state]})
+  }
+
+  objToList.sort((state1, state2) => state1.key.localeCompare(state2.key));
+  console.log('Отсортированный массив (по алфавиту)', objToList)
+
+  const resultDict = {}
+  
+  for (const stateIndex in objToList) {
+    let state = objToList[stateIndex].key
+    if (reference.includes(state)) {
+      let needIndex = reference.indexOf(state)
+      resultDict[state] = needIndex
+    } else {
+      let needIndexNonSort = Number(stateIndex) + Number(reference.length)
+      resultDict[state] = needIndexNonSort
+    }
+  }
+
+  objToList.sort((state1, state2) => resultDict[state1.key] - resultDict[state2.key]);
+  console.log('Отсортированный массив (по референсу)', objToList)
+
+  return objToList;
+}
